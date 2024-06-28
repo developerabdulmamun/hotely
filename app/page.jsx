f5d90cba44727253648777a8d11740f7918eac53
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-06-28 11:49:53 +0600

    Complete hero and search box

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,11 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-[1440px] mx-auto bg-white overflow-hidden">
       <Header />
       <Hero />
       <Footer />
+      <div className="h-[4000px]"></div>
     </main>
   );
 }
